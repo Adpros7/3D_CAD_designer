66917479e77cd8594236e52524d4a728b6f4e040
Adpros7 <pranit.advik@gmail.com>
2026-01-05 17:33:08 -0500

    Convert HTML documentation files to markdown format and add blender_docs_og to gitignore

--- indexer.py
+++ indexer.py
@@ -1,3 +1,4 @@
+import os
 from bs4 import BeautifulSoup
 import pyperclip
 
@@ -6,9 +7,9 @@ def extract_text_from_html(html):
     soup = BeautifulSoup(html, 'html.parser')
     return soup.find(id="furo-main-content")
 
-with open("test.txt", "w") as f:
-    with open("html_files/aud.html", "r", encoding="utf-8") as f:
-        fu = f.read()
-        print(extract_text_from_html(fu).text)
-        pyperclip.copy(extract_text_from_html(fu).text)
+os.mkdir("blender_docs") if not os.path.exists("blender_docs") else None
+for file in os.listdir("html_files"):
+    with open(f"C:\Users\prani\Coding\AI\3D_CAD_designer\html_files{file}", "r", encoding="utf-8") as fl:
+        fu = fl.read()
+    with open(f"blender_docs/{file.removesuffix('.html')}.md", "w") as f:
         f.write(extract_text_from_html(fu).text)
