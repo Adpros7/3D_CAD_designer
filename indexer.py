d324115cb4e87baa2e350e9b0569606aea1f716e
Adpros7 <pranit.advik@gmail.com>
2026-01-05 20:31:03 -0500

    Clean extracted HTML content by removing paragraph symbols before saving to markdown.

--- indexer.py
+++ indexer.py
@@ -7,9 +7,14 @@ def extract_text_from_html(html):
     soup = BeautifulSoup(html, 'html.parser')
     return soup.find(id="furo-main-content")
 
+
 os.mkdir("blender_docs") if not os.path.exists("blender_docs") else None
-for file in os.listdir("html_files"):
-    with open(f"C:\Users\prani\Coding\AI\3D_CAD_designer\html_files{file}", "r", encoding="utf-8") as fl:
+files = os.listdir("html_files")
+files = [file for file in files if file.endswith(".html")]
+for file in files:
+    with open(f"C:/Users/prani/Coding/AI/3D_CAD_designer/html_files/{file}", "r", encoding="utf-8") as fl:
         fu = fl.read()
-    with open(f"blender_docs/{file.removesuffix('.html')}.md", "w") as f:
-        f.write(extract_text_from_html(fu).text)
+    with open(f"C:/Users/prani/Coding/AI/3D_CAD_designer/html_files/{file}", "w", encoding="utf-8") as fll:
+        fll.write(fu.replace("¶", "", -1))
+    with open(f"blender_docs/{file.removesuffix('.html')}.md", "w", encoding="utf-8") as f:
+        f.write(extract_text_from_html(fu).text) # type: ignore
