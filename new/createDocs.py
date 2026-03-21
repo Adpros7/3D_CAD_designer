2a53d46452c84d949d25ffc715fb2a9afc1be937
Adpros7 <pranit.advik@gmail.com>
2026-03-21 16:19:50 -0400

    Add script to parse Blender Python reference documentation files.

---
+++ new/createDocs.py
@@ -0,0 +1,7 @@
+import os
+
+for i in os.listdir(r'C:\Users\prani\Coding\AI\3D_CAD_designer\new\blender_python_reference_5_1'):
+    print(i)
+    with open(f'C:/Users/prani/Coding/AI/3D_CAD_designer/new/blender_python_reference_5_1/{i}', 'r') as f:
+        data = f.read()
+        data = data.find(<article role="main" id="furo-main-content">)
